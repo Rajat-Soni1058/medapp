65fa7f9b3bc424917b03876ba511182cda90ae9d
urastogi2048 <utkarshrastogi2048@gmail.com>
2026-02-14 21:10:04 +0530

    env

--- medapp_backend/configuration/firebase.js
+++ medapp_backend/configuration/firebase.js
@@ -1,7 +1,18 @@
-const admin = require ('firebase-admin');
-const serviceAccount= require('../consultone-7e371-a4fb2afbc8d6.json');
+const admin = require('firebase-admin');
+
+let serviceAccount;
+
+// Check if running on Render (environment variable exists)
+if (process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON) {
+    // Parse JSON from environment variable
+    serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+} else {
+    // Local development - use file
+    serviceAccount = require('../consultone-7e371-a4fb2afbc8d6.json');
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
-module.exports=admin;
+
+module.exports = admin;
